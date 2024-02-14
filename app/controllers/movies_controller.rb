0ece55df5e3daed0e0579155735d66b55eb9de13
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-02-13 18:29:35 -0700

    Add authorize function to movie controller to check for session user id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,19 @@
 class MoviesController < ApplicationController
   def index 
-      @user = User.find(params[:id])
-      @movies = Movie.all
+    @user = User.find(params[:id])
+    @movies = Movie.all
   end 
 
   def show 
+    user = User.find(params[:user_id])
+    movie = Movie.find(params[:id])
+    if authorize(user.id)
       @user = User.find(params[:user_id])
       @movie = Movie.find(params[:id])
+    else
+      flash[:error] = "You must be logged in or registered to create a viewing party"
+      render :show
+      # redirect_to "/users/#{user.id}/movies/#{movie.id}"
+    end
   end 
 end 
